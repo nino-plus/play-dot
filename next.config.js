0785bf98b7c1fcd4ac4a23c3956504792a5ddf5b
Daichi Ninomiya <daichi.ninomiya@deer.co.jp>
2024-03-02 04:23:02 +0900

    fix

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   output: 'export',
   basePath: process.env.REPOSITORY_NAME,
   env: {
-    repo: REPOSITORY_NAME,
+    repo: process.env.REPOSITORY_NAME,
   },
   images: {
     unoptimized: true,
